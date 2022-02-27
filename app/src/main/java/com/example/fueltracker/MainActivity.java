3d5a032cc1737b0ef6faf8b407123ba219349700
Milan <milan.338@outlook.com>
2022-02-27 13:23:05 +1030

    Update launcher icon

--- app/src/main/java/com/example/fueltracker/MainActivity.java
+++ app/src/main/java/com/example/fueltracker/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
